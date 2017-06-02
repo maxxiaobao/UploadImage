91a521ec76e2d6c6ff3962d10fc52bbc3e159fd3
maxxiaobao <maxiuli95@gmail.com>
2017-06-02 11:12:00 +0800

    Draw out a file

---
+++ formatImg.js
@@ -0,0 +1,38 @@
+function getFormatImgURL(file, callback){
+	EXIF.getData(file, function() {
+	  var orientation = EXIF.getTag(this,"Orientation");
+	  var can = document.createElement("canvas");
+	  var ctx = can.getContext('2d');
+	  var thisImage = new Image;
+	  thisImage.onload = function() {
+	    can.width  = thisImage.width;
+	    can.height = thisImage.height;
+	    ctx.save();
+	    var width  = can.width;  var styleWidth  = can.style.width;
+	    var height = can.height; var styleHeight = can.style.height;
+	    if (orientation) {
+	      if (orientation > 4) {
+	        can.width  = height; can.style.width  = styleHeight;
+	        can.height = width;  can.style.height = styleWidth;
+	      }
+	      switch (orientation) {
+		      case 2: ctx.translate(width, 0);     ctx.scale(-1,1); break;
+		      case 3: ctx.translate(width,height); ctx.rotate(Math.PI); break;
+		      case 4: ctx.translate(0,height);     ctx.scale(1,-1); break;
+		      case 5: ctx.rotate(0.5 * Math.PI);   ctx.scale(1,-1); break;
+		      case 6: ctx.rotate(0.5 * Math.PI);   ctx.translate(0,-height); break;
+		      case 7: ctx.rotate(0.5 * Math.PI);   ctx.translate(width,-height); ctx.scale(-1,1); break;
+		      case 8: ctx.rotate(-0.5 * Math.PI);  ctx.translate(-width,0); break;
+	      }
+	    }
+	    ctx.drawImage(thisImage,0,0);
+	    ctx.restore();
+	    var dataURL = can.toDataURL();
+	    if (callback) {
+	    	callback(dataURL)
+	    }
+	    // at this point you can save the image away to your back-end using 'dataURL'
+	  }
+	  thisImage.src = URL.createObjectURL(file);
+	});
+}
